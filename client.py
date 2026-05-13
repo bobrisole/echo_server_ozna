eefd9e1983e17cc9b8306057d165f122e4c9e390
Robert Rezyapov <Robert-bek@ya.ru>
2026-05-13 16:13:02 +0500

    All: добавил разные функции и зависимости

--- client.py
+++ client.py
@@ -1,14 +1,38 @@
 import requests
 import random
+import json
 
-data = {}
-for number in range(1, 1001):
-    data[f"number_{number}"] = random.randint(1, 10) * random.randint(1, 10)
+def generate_random_data(number):
+    data = {}
+    for i in range(1, number + 1):
+        data[f"number_{i}"] = random.randint(1, 10) * random.randint(1, 10)
+    return data
 
-# Отправляем POST-запрос
+def limit_for_generate_random_data():
+    input_number = input("Введите число от 1 до 1000 для генерации данных: ")
+    number = int(input_number)
+    if 1 <= number <= 1000:
+        return number
+    else:
+        print("Числло вне диапазона.")
+        return limit_for_generate_random_data()
+
+def extract_number(key):
+    parts = key.split("_")
+    number_string = parts[1]
+    number = int(number_string)
+    return number
+
+def print_sorted_data(data):
+    sorted_data = {}
+    for key in sorted(data.keys(), key=extract_number):
+        sorted_data[key] = data[key]
+    print(json.dumps(sorted_data, indent=4)) #   Пасхалка   ,`'- o==3
+
+data = generate_random_data(limit_for_generate_random_data())
 response = requests.post(
     "http://127.0.0.1:5000/sendData",
     json=data
 )
 
-print(response.json())
+print_sorted_data(data)
